bb4ca26d79e26d232e5995f528a55584b1423573
Aryan <aryan2563737@gmail.com>
2020-11-25 22:10:32 +0530

    finished

--- canvas.js
+++ canvas.js
@@ -15,47 +15,66 @@ const randColor = () => {
     }
     return 'rgba(' + color.r * 255 + ',' + color.g * 255 + ',' + color.b * 255 + ',' + color.a + ')';
 };
+const mouse = {
+    x: innerWidth / 2,
+    y: innerHeight / 2
+};
+addEventListener('mousemove', e => {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+});
 const canvas = document.querySelector('canvas');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 const c = canvas.getContext('2d');
-const Particle = function (x, y, radius, color, distance, speed) {
+const Particle = function (x, y, radius, color) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
-    this.radians = 0;
-    this.distance = distance;
-    this.speed = speed;
+    this.radians = Math.random() * (Math.PI * 2);
+    this.distance = (Math.random() * 50) + 70;
+    this.speed = 0.05;
+    this.prevMouse = {
+        x: x,
+        y: y
+    };
     this.draw = () => {
         c.beginPath();
-        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        c.fillStyle = this.color;
-        c.fill();
+        c.strokeStyle = this.color;
+        c.lineWidth = this.radius;
+        c.moveTo(this.prevPosition.x, this.prevPosition.y);
+        c.lineTo(this.x, this.y);
+        c.stroke();
         c.closePath();
     };
     this.magic = () => {
+        this.prevPosition = {
+            x: this.x,
+            y: this.y
+        };
+        this.prevMouse.x += (mouse.x - this.prevMouse.x) * 0.05;
+        this.prevMouse.y += (mouse.y - this.prevMouse.y) * 0.05;
         this.radians += this.speed;
-        this.x = x + Math.cos(this.radians) * this.distance;
-        this.y = y + Math.sin(this.radians) * this.distance;
+        this.x = this.prevMouse.x + Math.cos(this.radians) * this.distance;
+        this.y = this.prevMouse.y + Math.sin(this.radians) * this.distance;
         this.draw();
     };
 };
 const particles = [];
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 200; i++) {
     const x = innerWidth / 2;
     const y = innerHeight / 2;
-    const radius = 5;
+    const radius = (Math.random() * 3) + 2;
     const color = randColor();
-    const distance = (Math.random() * 10) * 40;
-    const speed = (Math.random() - 0.5) / 2;
-    const particle = new Particle(x, y, radius, color, distance, speed / 10);
+    const particle = new Particle(x, y, radius, color);
     particles.push(particle);
 }
 console.log(particles);
 const animate = () => {
     requestAnimationFrame(animate);
-    c.clearRect(0, 0, innerWidth, innerHeight);
+    c.fillStyle = "rgba(0,0,0,0.05)";
+    c.fillRect(0, 0, innerWidth, innerHeight);
     particles.forEach(particle => {
         particle.magic();
     });
